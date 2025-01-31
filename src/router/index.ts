187d7c016d9315f0324976845bee10daf0c836ca
Statesboro Pride <admin@statesboropride.org>
2025-01-31 09:36:29 -0500

    Added `NavLink` to exports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,3 @@
-export { BrowserRouter, Routes, Route, Outlet, useLocation, useNavigate } from "react-router";
+export { BrowserRouter, NavLink, Routes, Route, Outlet, useLocation, useNavigate } from "react-router";
 export { default as Redirect } from "./Redirect";
 export { default as useRedirect } from "./useRedirect";
