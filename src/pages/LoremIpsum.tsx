87ac1b1ebb2df064b369cd82123ac9a06f0cbccf
Statesboro Pride <admin@statesboropride.org>
2025-01-31 09:45:10 -0500

    Added test content component

---
+++ src/pages/LoremIpsum.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function LoremIpsum() {
+  return (
+    <React.Fragment>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum mollis sapien, vel viverra odio
+        convallis nec. Phasellus laoreet nunc eu massa pharetra placerat. Quisque id odio in magna euismod sollicitudin
+        quis eget libero. Sed semper, ex ut fringilla fringilla, purus nunc dictum lorem, quis dignissim metus erat eget
+        nisl. Vivamus massa ante, facilisis in lobortis lobortis, lacinia vel libero. Nunc auctor dolor a lorem
+        lobortis, at congue est maximus. Sed ac elit at velit laoreet convallis sit amet ac purus. Cras urna nunc,
+        elementum eu euismod condimentum, commodo et urna.
+      </p>
+      <p>
+        Quisque consectetur sollicitudin leo, id dignissim magna dapibus quis. Phasellus pretium nulla et purus congue,
+        et laoreet quam aliquam. Nullam eu est accumsan, elementum ipsum at, blandit metus. Class aptent taciti sociosqu
+        ad litora torquent per conubia nostra, per inceptos himenaeos. Donec sed enim et purus consequat euismod vel in
+        est. Aliquam ac imperdiet eros. Phasellus eget laoreet dolor, eget suscipit neque. Vestibulum non tempor augue.
+        Etiam sit amet tellus ante. Etiam dignissim tempus gravida.
+      </p>
+      <p>
+        Etiam erat dolor, fringilla eget blandit ac, tempor eget lectus. Vivamus iaculis eget libero sit amet pharetra.
+        Nunc at sem sed felis ultricies porttitor sed sit amet mi. Nullam et nunc dapibus, bibendum odio ac, porta mi.
+        Maecenas nec porta nisi. Proin aliquam lobortis vestibulum. Pellentesque non bibendum augue. Sed pretium id mi
+        vitae aliquet. Quisque lacinia lorem sit amet neque rhoncus posuere. Pellentesque pretium at diam eget porta.
+      </p>
+      <p>
+        Aenean euismod vitae orci nec tincidunt. Mauris finibus velit at tellus aliquet vulputate. Praesent vel enim in
+        diam pretium pulvinar. Sed tempor, justo sit amet vestibulum tristique, ex mi placerat massa, nec efficitur
+        purus leo eget felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Nunc sagittis odio metus, quis placerat nibh tempus a. Praesent commodo tristique est, et malesuada erat
+        eleifend at. Vestibulum nunc augue, molestie in sodales nec, volutpat molestie nisi. Aenean sed enim lorem. Nunc
+        dignissim ultrices dictum.
+      </p>
+      <p>
+        Aliquam id neque ut enim feugiat gravida. Cras sodales facilisis nunc id finibus. Curabitur felis quam, egestas
+        vitae orci non, sollicitudin vulputate massa. Etiam feugiat dictum eros in malesuada. Donec sed lacus ex. Donec
+        dictum eros diam, non malesuada dui fermentum at. Praesent sit amet leo dictum augue tempus varius.
+      </p>
+    </React.Fragment>
+  );
+}
