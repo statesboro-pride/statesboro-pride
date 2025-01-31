17f04d1f7bec3643fcefa261610cf1dd5890ff80
Statesboro Pride <admin@statesboropride.org>
2025-01-31 09:47:04 -0500

    Added a not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,14 +6,16 @@ export default function Home() {
   return (
     <React.Fragment>
       <PageHeader imgSrc="/images/header-main.jpg">
-        <h1>Girl, what are you doing?</h1>
+        <h1>We see you, but we don't see this page.</h1>
       </PageHeader>
       <PageContents>
         <p>We couldn't find the page you were looking for. This is either because:</p>
-        <ul>
-          <li>There is an error in the URL entered into your web browser. Please check the URL and try again.</li>
-          <li>The page you are looking for has been moved or deleted.</li>
-        </ul>
+        <p>
+          <ul>
+            <li>There is an error in the URL entered into your web browser. Please check the URL and try again.</li>
+            <li>The page you are looking for has been moved or deleted.</li>
+          </ul>
+        </p>
         <p>
           You can return to our homepage by <a href="/">clicking here</a>.
         </p>
