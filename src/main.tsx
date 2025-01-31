ff86c215677272a095d9c7b453608b00678027aa
Statesboro Pride <admin@statesboropride.org>
2025-01-31 09:52:30 -0500

    Moved `<App/>` into main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Config from "./config";
+import PageLayout from "./layout/PageLayout";
+import Page from "./pages";
+import { BrowserRouter, Redirect, Routes, Route } from "./router";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {[...Config.redirects].map(([path, redirect]) => (
+          <Route key={path} path={path} element={<Redirect to={redirect} />} />
+        ))}
+
+        <Route element={<PageLayout />}>
+          {[...Config.navs]
+            .filter(([, nav]) => nav.type === "page")
+            .map(([key, nav]) => (
+              <Route key={key} path={nav.path} element={<Page name={nav.label} />} />
+            ))}
+          <Route path="*" element={<Page name="Not Found" />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
