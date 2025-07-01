73f4a6216a8d01851fb4123a36416c4cc6f84e45
Statesboro Pride <admin@statesboropride.org>
2025-07-01 19:18:06 -0400

    Updated sponsorship flyer

--- src/pages/Partnerships/Sponsor.tsx
+++ src/pages/Partnerships/Sponsor.tsx
@@ -1,12 +1,6 @@
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import type { CSSProperties } from "react";
-import type { ResponsiveStyleValue } from "@mui/system";
 
-const stackDirection: ResponsiveStyleValue<"column" | "row" | "row-reverse" | "column-reverse"> = {
-  xs: "column",
-  lg: "row",
-};
 const imgStyle: CSSProperties = { maxWidth: "100%", objectFit: "contain" };
 
 export default function Sponsor() {
@@ -21,14 +15,9 @@ export default function Sponsor() {
           contact us
         </a>
       </p>
-      <Stack direction={stackDirection} spacing={1}>
-        <div>
-          <img src="/images/Sponsorship_Packet_1.webp" style={imgStyle} />
-        </div>
-        <div>
-          <img src="/images/Sponsorship_Packet_2.webp" style={imgStyle} />
-        </div>
-      </Stack>
+      <div>
+        <img src="/images/Sponsorship_Packet_1.webp" style={imgStyle} />
+      </div>
     </>
   );
 }
